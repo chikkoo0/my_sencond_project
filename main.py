6ca930cd6ba554f1a8f60c8b35bd231232b80b07
chikkoo0 <betaholdik@gmail.com>
2025-06-11 19:14:01 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+day = input("Введите число: ")
+
+if day == "1":
+    print("Понедельник")
+
+elif day == "2":
+    print("Вторник")
+
+elif day == "3":
+    print("Среда")
+
+elif day == "4":
+    print("Четверг")
+
+elif day == "5":
+    print("Пятница ")
+
+elif day == "6":
+    print("Суббота")
+
+elif day == "7":
+    print("Воскрисение")
+
+else:
+    print("Ошибка!")
+
+
